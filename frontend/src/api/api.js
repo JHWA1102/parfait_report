da553a467215f97130c0e49d1ba118585a5a1eb8
jhwa11020 <jhwa11020@gmail.com>
2025-12-07 21:58:17 +0900

    test

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // Spring Boot 서버 주소
+  baseURL: "http://52.78.47.157", // Spring Boot 서버 주소
   timeout: 5000,
 });
 
