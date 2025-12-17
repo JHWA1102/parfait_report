62ca47a09a8e334b81338c7450271baaede35e85
jhwa11020 <jhwa11020@gmail.com>
2025-12-17 22:28:32 +0900

    aws backend connection

--- backend/src/main/java/com/joy/parfaitReport/common/controller/CommonCotroller.java
+++ backend/src/main/java/com/joy/parfaitReport/common/controller/CommonCotroller.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,8 @@ import lombok.RequiredArgsConstructor;
 public class CommonCotroller {
 	private final CommonService commonService;
 	
-	@GetMapping("/api/common/code")
+	@RequestMapping("/common")
+	@GetMapping("/code")
     public List<Map<String, Object>> getCodeList(
             @RequestParam("mst_code") String mst_code) {
 
