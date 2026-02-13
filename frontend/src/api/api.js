608474d81aff7553653bd97d57b0f2faa2ffe9b7
Oracle Public Cloud User <opc@parfait-report-instance.subnet02032007.vcn02032007.oraclevcn.com>
2026-02-13 11:20:33 +0000

    server latest version

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://www.parfait-report.co.kr", // Spring Boot 서버 주소
+  baseURL: "/api", // Spring Boot 서버 주소
   timeout: 5000,
 });
 
--- frontend/src/api/commonApi.js
+++ frontend/src/api/commonApi.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 // 코드 리스트 조회
 export const getCodeList = async (mstCode) => {
-  const response = await api.get("/api/common/code", {
+  const response = await api.get("/common/code", {
     params: { mst_code: mstCode },
   });
   return response.data;
